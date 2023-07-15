03ef9bf95f1648db93618a6cf1489206241ae52d
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-07-15 16:44:24 +0500

    images added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,11 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="assets/css/swiper.css" />
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <link rel="manifest" href="manifest.json">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
     <title>{{ env('APP_NAME') }}</title>
 </head>
 
@@ -34,35 +34,31 @@
                     <div class="swiper-wrapper">
                         <div class="swiper-slide d-flex justify-content-center">
                             <div class="box">
-                                <img src="assets/img/slide1.png" class="img-fluid" alt="">
+                                <img src="{{ asset('assets/img5.jpg') }}" class="img-fluid" alt="">
                                 <h1 class="mt-5">{{ env('APP_NAME') }}</h1>
                                 <p>Invest with us, earn with us!</p>
                             </div>
                         </div>
                         <div class="swiper-slide d-flex justify-content-center">
                             <div class="box">
-                                <img src="assets/img/slide2.png" class="img-fluid" alt="">
+                                <img src="{{ asset('assets/img6.jpg') }}" class="img-fluid" alt="">
                                 <h1 class="mt-5">{{ env('APP_NAME') }}</h1>
-                                <p>With allthe information you need, you can start playing and winning. Good Luck!
+                                <p>With all the information you need, you can start investing and earning. Good Luck!
                                 </p>
                             </div>
                         </div>
                         <div class="swiper-slide d-flex justify-content-center">
                             <div class="box">
-                                <img src="assets/img/slide3.png" class="img-fluid" alt="">
+                                <img src="{{ asset('assets/img7.jpg') }}" class="img-fluid" alt="">
                                 <h1 class="mt-5">{{ env('APP_NAME') }}</h1>
-                                <p>Feel the exuberance and excitement of playing to win in this gaming environment right
-                                    through your
-                                    mobile devices</p>
+                                <p>Feel the exuberance and excitement of inviting new friends and earn profit.</p>
                             </div>
                         </div>
                         <div class="swiper-slide d-flex justify-content-center">
                             <div class="box">
-                                <img src="assets/img/slide4.png" class="img-fluid" alt="">
+                                <img src="{{ asset('assets/img4.jpg') }}" class="img-fluid" alt="">
                                 <h1 class="mt-5">{{ env('APP_NAME') }}</h1>
-                                <p>Ready to challenge players from all over the world in the ultimate betting contest?
-                                    Bet for FREE
-                                    Sports and eSports and compete for 12 MILLION Bether in prizes!</p>
+                                <p>Investe with us and invite more friends to our platform to win exciting prizes!</p>
                             </div>
                         </div>
 
@@ -78,11 +74,11 @@
     </section>
 
     <!-- JS file linkes -->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/swiper.min.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/service-worker-settings.js"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/swiper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ asset('assets/js/service-worker-settings.js') }}"></script>
 </body>
 
 </html>
