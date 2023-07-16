03b44eedab4777b33845a12319408b9ea88dddb0
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-07-16 18:04:42 +0500

    asdf

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -49,7 +49,7 @@
                 <li class="nav-item" style="flex: 0 0 calc(15% - 10px);" role="presentation">
                     <button class="nav-link active"><i class="bi bi-circle-fill live"></i>
                         Recharge</button>
-                </li>
+                </li>asdf
             </a>
             <a href="{{ route('User.Widthraw') }}">
                 <li class="nav-item" style="flex: 0 0 calc(15% - 10px);" role="presentation">
@@ -63,8 +63,15 @@
             </a>
         </ul>
     </section>
+    <div class="container" style="margin-top: -40px">
+        <div class="row">
+            <div class="col-sm-12">
+                <hr style="color:white;width:100%;text-align:center;">
+            </div>
+        </div>
+    </div>
 
-    <div class="container mb-3" style="margin-top: -30px">
+    <div class="container mb-3" style="margin-top: -40px">
         <h4>New Games</h4>
         <div class="row autoplay">
             @forelse ($games as $game)
@@ -84,16 +91,9 @@
         </div>
     </div>
 
-    <div class="container" style="margin-top: -40px">
-        <div class="row">
-            <div class="col-sm-12">
-                <hr style="color:white;width:100%;text-align:center;">
-            </div>
-        </div>
-    </div>
 
     <div class="container mb-5" style="margin-bottom: 80px;margin-top:-30px;">
-    <h4>All Games</h4>
+        <h4>All Games</h4>
         <div class="row autoplay">
             @forelse ($games as $game)
                 <div class="col-sm-4 px-1">
